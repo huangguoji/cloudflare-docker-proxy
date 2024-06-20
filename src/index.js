4e3d05a16dbe9721bc6303d969a79d87e643a35e
huangguoji <huangguoji@126.com>
2024-06-20 14:38:24 +0800

    update domain

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.cage365.top": dockerHub,
+  "quay.cage365.top": "https://quay.io",
+  "gcr.cage365.top": "https://gcr.io",
+  "k8s-gcr.cage365.top": "https://k8s.gcr.io",
+  "k8s.cage365.top": "https://registry.k8s.io",
+  "ghcr.cage365.top": "https://ghcr.io",
+  "cloudsmith.cage365.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.cage365.top": dockerHub,
 };
 
 function routeByHosts(host) {
